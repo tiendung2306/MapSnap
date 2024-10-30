0597246fec46f243c8d55b3e2497208d69768df7
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2024-10-31 01:28:27 +0700

    Revert "Fix: Refactoring bugs"

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,8 +1,6 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Journey = require('./journey.model');
-module.exports.Visit = require('./visit.model');
 module.exports.Location = require('./location.model');
 module.exports.Position = require('./position.model');
 module.exports.Picture = require('./picture.model');
-module.exports.VerificationPin = require('./verificationPin.model');
