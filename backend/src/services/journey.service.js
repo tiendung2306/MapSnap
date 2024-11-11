4e788eb39cbd1236126086ed6d72b83c1fdc54aa
Khai Tran <tranmanhkhai2004@gmail.com>
2024-11-11 18:03:21 +0700

    feat: today

--- backend/src/services/journey.service.js
+++ backend/src/services/journey.service.js
@@ -1,27 +1,28 @@
 const httpStatus = require('http-status');
 const mongoose = require('mongoose');
 // const _ = require('lodash');
-const JourneyModel = require('../models/journey.model');
+const Journey = require('../models/journey.model');
 const ApiError = require('../utils/ApiError');
 const Message = require('../utils/Message');
 // const UserModel = require('../models/user.model');
 
 const createJourney = async (journeyBody) => {
-  const journey = await JourneyModel.findOne(journeyBody);
+  const journey = await Journey.findOne(journeyBody);
   if (journey) {
     throw new ApiError(httpStatus.BAD_REQUEST, Message.journeyMsg.nameExisted);
   }
-  const journeyModel = await JourneyModel.create(journeyBody);
+  const journeyModel = await Journey.create(journeyBody);
   return journeyModel;
 };
 
 const getJourneysByUserId = async ({ userId }) => {
-  const filter = { userId: mongoose.Types.ObjectId(userId), status: { $ne: 'enable' } };
-  return JourneyModel.find(filter);
+  const filter = { userId: mongoose.Types.ObjectId(userId) };
+  const journeys = await Journey.find(filter);
+  return journeys;
 };
 
 const getJourneyByJourneyId = async (journeyId) => {
-  const journey = await JourneyModel.findById(journeyId);
+  const journey = await Journey.findById(journeyId);
   if (!journey) {
     throw new ApiError(httpStatus.NOT_FOUND, Message.notFound);
   }
@@ -29,12 +30,28 @@ const getJourneyByJourneyId = async (journeyId) => {
 };
 
 const updateJourney = async ({ journeyId, requestBody }) => {
-  const journeyModel = await JourneyModel.findByIdAndUpdate(journeyId, requestBody, { new: true });
+  const journeyModel = await Journey.findOneAndUpdate(journeyId, requestBody, { new: true });
   return journeyModel;
 };
 
 const deleteJourney = async (journeyId) => {
   await updateJourney({ journeyId, requestBody: { status: 'disabled' } });
+};
+
+const getJourneysToday = async (userId) => {
+  let startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+  startOfDay = startOfDay.getTime();
+  let endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+  endOfDay = endOfDay.getTime();
+  const filter = {
+    userId: mongoose.Types.ObjectId(userId),
+    startedAt: { $gte: startOfDay, $lte: endOfDay },
+  };
+
+  const journeys = await Journey.find(filter);
+  return journeys;
 };
 
 module.exports = {
@@ -43,4 +60,5 @@ module.exports = {
   getJourneyByJourneyId,
   updateJourney,
   deleteJourney,
+  getJourneysToday,
 };
